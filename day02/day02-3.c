a65515940545f3e876b4f55b82e0eb96c0d3902b
seoyun <s471y@kakao.com>
2024-09-12 14:31:13 +0900

    2024.09.12
    3과 4의 공배수 또는 7의 배수 출력

---
+++ day02/day02-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+
+    for(i=0;i<=99;i++){
+        if(i%3==0&&i%4==0||i%7==0){
+            printf("%d ", i);
+        }
+    }
+    
+    return 0;
+}
