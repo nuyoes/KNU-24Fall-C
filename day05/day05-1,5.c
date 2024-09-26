75c14fa0321ab10dc7002520fbeab34f6ca85498
nuyoes <77190970+nuyoes@users.noreply.github.com>
2024-09-26 14:43:20 +0900

    240926
    구조체 쓰는 방법을 배우다......

---
+++ day05/day05-1,5.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Student{
+	int snum;
+	char *name;
+	int score;
+};
+
+int main() {
+	int N;
+	struct Student person[2];
+
+	person[0].snum = 1111; 
+	person[0].name = (char*)malloc(sizeof(char) * 50); //필요한 만큼 할당.. 아어렵다
+	strcpy_s(person[0].name, 50, "철수");
+	person[0].score = 100;
+
+	printf("%d %s %d", person[0].snum, person[0].name, person[0].score);
+	//구조체 값 접근하기 위해 점(.)을 쓴다...
+	
+	return 0;
+}
