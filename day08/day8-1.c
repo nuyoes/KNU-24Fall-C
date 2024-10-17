543dd5196a7fb27cabf9652957d8843d9901f861
nuyoes <s471y@kakao.com>
2024-10-17 14:51:36 +0900

    241017
    아직 해결 못 함;;;;

---
+++ day08/day8-1.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+// 1차 저장... 선배가 알려준 걸 보자...
+
+void reverse(char *s) {
+	int len = strlen(s) - 1; //마지막에 공백이 저장되니 길이를 하나 빼 줌
+	char* p = len + s;  //모르게슨데?
+
+	while (p >= s) {
+		printf("%c", *p);
+		p--;
+	}
+}
+
+int main() {
+	// string 포인터 하나만 만들어서 얠 내부에서 뒤집으래... 2개 만들어서 복사하는 거 아님
+	char* s[100];
+
+	scanf_s("%s", &s, sizeof(s));
+
+	reverse(s);
+
+	return 0;
+}
---
+++ day08/day8-2.c
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#define len 10
+
+// 1차 저장...
+
+int arr[len][len];
+
+void fillRandom() {
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++)
+			arr[i][j] = rand() % 20 + 1;
+	}
+}
+
+void printArray() {
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++)
+			printf("%d ", arr[i][j]);
+		printf("\n");
+	}
+
+}
+ 
+//여기 수정해야 댐
+void* movePointer(void* arr) { //왜 void형 포인터를 사용할까...? int..면안되나? 포인터 매개변수를 잘 모르겐는데
+	int* arrPointer = arr; //배열의 첫 번째 주소
+	int i = 0, j = 0;
+
+	printf("현재 위치 : (0, 0), 배열의 값 : % d)", *arrPointer);
+	
+	while (1) {
+		if (arrPointer <= 100) {
+			printf("현재 위치 : (% d, % d), 배열의 값 : % d)", i, j, *(arrPointer + 1); //킵킵킵
+		}
+		else break;
+	}
+	
+}
+
+
+int main() {
+	//기본 코드?
+	fillRandom();
+	printArray();
+	movePointer(arr);
+
+
+
+	return 0;
+}
